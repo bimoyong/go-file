1e5d3c359f7159cf6462b9d23bdfad2c51217ba9
Trung Bui <bi.moyong@gmail.com>
2021-09-03 17:31:29 +0700

    refactor

--- handler_test/upload_test.go
+++ handler_test/upload_test.go
@@ -10,7 +10,6 @@ import (
 	"time"
 
 	"github.com/micro/go-micro/v2/client"
-	log "github.com/micro/go-micro/v2/logger"
 	"github.com/micro/go-micro/v2/metadata"
 
 	proto "github.com/bimoyong/go-file/proto/file"
@@ -19,69 +18,73 @@ import (
 
 // TestUpload function
 func TestUpload(t *testing.T) {
-	var err error
-
 	go func() {
-		if err = test.StartService(); err != nil {
-			t.Error(err)
+		if err := test.StartService(); err != nil {
+			t.Errorf("Error starting service! %s", err.Error())
 		}
 	}()
 	time.Sleep(time.Second * 2)
 
 	ctx := metadata.Set(context.TODO(), "Domain", "staging")
 	ctx = metadata.Set(ctx, "Alias", "vehicles")
+	md, _ := metadata.FromContext(ctx)
 
-	f, _ := os.Open("../resource_test/12032006.jpeg")
 	req := proto.UploadReq{
 		Checksum: "todo_checksum",
 	}
+	t.Logf("Setup! metadata=[%+v] req=[%s]", md, req.String())
 
-	if err = upload(ctx, &req, f); err != nil {
-		t.Error(err)
-	}
-	t.Logf("Test done")
-}
-
-func upload(ctx context.Context, req *proto.UploadReq, rd io.Reader) (err error) {
 	srv := proto.NewFileService(test.ServerName, client.DefaultClient)
-	var strm proto.File_UploadService
-	if strm, err = srv.Upload(ctx); err != nil {
-		err = fmt.Errorf("failed to upload file: %s", err.Error())
-		return
+	strm, err := srv.Upload(ctx)
+	if err != nil {
+		t.Fatalf("Error handshaking upload service! %s", err.Error())
 	}
 	defer strm.Close()
 
-	var resp proto.UploadResp
+	fname := "../resource_test/12032006.jpeg"
+	f, err := os.Open(fname)
+	if err != nil {
+		t.Fatalf("Error opening file %s! %s", fname, err.Error())
+	}
+	defer f.Close()
 
-	// sent data in 1M chunks
 	buf := make([]byte, 1<<20)
+	size := int64(0)
+	resp := proto.UploadResp{}
 
-	log.Infof("Stream file for every chunk size %d", 1<<20)
+	t.Logf("Start uploading file for every chunk size %d", cap(buf))
 	for {
-		var n int
-		if n, err = rd.Read(buf); err == io.EOF {
-			err = nil
-			log.Infof("Finished uploading file")
+		n, err := f.Read(buf)
+		if err == io.EOF {
 			break
 		}
 		if err != nil {
-			err = fmt.Errorf("error reading file: %s", err.Error())
-			break
+			t.Fatalf("Error reading file! %s", err.Error())
 		}
 
 		req.Data = buf[:n]
-		req.Checksum = fmt.Sprintf("%x", sha1.Sum(buf[:n]))
-		if err = strm.Send(req); err != nil {
-			err = fmt.Errorf("error uploading file: %s", err.Error())
-			break
+		req.Checksum = fmt.Sprintf("%x", sha1.Sum(req.Data))
+		if err = strm.Send(&req); err != nil {
+			t.Fatalf("Error uploading! %s", err.Error())
 		}
-		log.Infof("Streamed %d bytes of file", n)
+
+		size += int64(n)
+		t.Logf("Uploaded %d bytes", size)
 
 		if err = strm.RecvMsg(&resp); err != nil {
-			return
+			t.Fatalf("Error ack uploading! %s", err.Error())
 		}
 	}
-	log.Infof("Received response %s", resp.String())
 
-	return
+	t.Logf("Server responds %s", resp.String())
+
+	finfo, err := f.Stat()
+	if err != nil {
+		t.Fatalf("Error stating file! %s", err.Error())
+	}
+	if finfo.Size() != size {
+		t.Fatalf("File is not uploaded fully! Expect %d bytes", finfo.Size())
+	}
+
+	t.Logf("Teardown!")
 }
