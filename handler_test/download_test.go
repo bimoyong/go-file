e728815ed6d13ec5f64ff1da5d63f491f0d69965
Trung Bui <bi.moyong@gmail.com>
2021-09-03 16:39:20 +0700

    implement test case for File.Download

---
+++ handler_test/download_test.go
@@ -0,0 +1,93 @@
+package handler_test
+
+import (
+	"context"
+	"crypto/sha1"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	_ "github.com/micro/go-micro/v2"
+	"github.com/micro/go-micro/v2/client"
+	"github.com/micro/go-micro/v2/metadata"
+
+	proto "github.com/bimoyong/go-file/proto/file"
+	test "github.com/bimoyong/go-file/test"
+)
+
+// TestDownload function
+func TestDownload(t *testing.T) {
+	go func() {
+		if err := test.StartService(); err != nil {
+			t.Error(err)
+		}
+	}()
+	time.Sleep(time.Second * 2)
+
+	ctx := metadata.Set(context.TODO(), "Domain", "staging")
+	ctx = metadata.Set(ctx, "Alias", "vehicles")
+	md, _ := metadata.FromContext(ctx)
+
+	req := proto.DownloadReq{
+		Id: "todo_id",
+	}
+	t.Logf("Setup! metadata=[%+v] req=[%s]", md, req.String())
+
+	srv := proto.NewFileService(test.ServerName, client.DefaultClient)
+	strm, err := srv.Download(ctx, &req)
+	if err != nil {
+		t.Fatalf("Failed to download file! %s", err.Error())
+	}
+	defer strm.Close()
+
+	size := 0
+	resp := proto.DownloadResp{}
+
+	fname := filepath.Join(os.TempDir(), fmt.Sprintf("%s.jpg", req.Id))
+	f, err := os.Create(fname)
+	if err != nil {
+		t.Fatalf("Error creating file %s! %s", fname, err.Error())
+	}
+	defer f.Close()
+
+	t.Logf("Start downloading file %s", fname)
+	for {
+		err := strm.RecvMsg(&resp)
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			t.Fatalf("Error downloading file! %s", err.Error())
+		}
+
+		n, err := f.Write(resp.Data)
+		if err != nil {
+			t.Fatalf("Error writing file: %s", err.Error())
+		}
+		checksum := fmt.Sprintf("%x", sha1.Sum(resp.Data))
+		if checksum != resp.Checksum {
+			t.Fatalf("Incorrect checksum! Expect %s but given %s", checksum, resp.Checksum)
+		}
+
+		size += n
+		t.Logf("Downloaded %d bytes of file", size)
+	}
+
+	finfo, err := f.Stat()
+	if err != nil {
+		t.Fatalf("Error stating file! %s", err.Error())
+	}
+	t.Logf("File size: %d bytes", finfo.Size())
+
+	if size != 577855 {
+		t.Errorf("File is not downloaded fully! Expect %d bytes", 577855)
+	}
+
+	err = os.Remove(fname)
+	if err != nil {
+		t.Fatalf("Error delete file! %s", err.Error())
+	}
+	t.Logf("Teardown!")
+}
